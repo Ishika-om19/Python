5bffa6090c42e8ca3e559ecc0f0e354613a66ff3
Ishika-om19 <somi37987@gmail.com>
2026-08-31 14:41:43 +0530

    Adding Tkinter Combobox and Messagebox

---
+++ UI/combobox_messagebox.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox
+
+def show_selected(x):
+    selected = mylist.get()
+    cname = text1.get().strip().title()
+    messagebox.showinfo("Message",cname + " , Your Test Center will be at " + selected)
+
+mywin=Tk()#this line creating window mywin.title("New Window")
+mywin.title("New Window")
+mywin.geometry("400x400")
+mywin.config(bg="black")
+
+label1 = ttk.Label(mywin, text="Enter Your Name")#Creating a Label
+text1 = ttk.Entry (mywin) #Creates An Edit Box
+label1.pack()
+text1.pack() #To Make the controlVisible
+
+label2 = ttk.Label(mywin, text="Enter your Mobile No")#Creating a Label
+text2 = ttk.Entry(mywin) #Creates An Edit Box
+label2.pack()
+text2.pack()
+
+label3 = tk.Label(mywin, text="Enter Your Location", fg="Green")#Creating a Lable 
+citylist = ["New Delhi", "Meerut", "Ghaziabad", "Bhopal", "Kanpur", "Noida"]
+mylist = ttk.Combobox(mywin,values=citylist)
+mylist.bind("<<ComboboxSelected>>",show_selected)
+label3.pack()
+mylist.pack()
+mywin.mainloop()
