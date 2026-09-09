47754059f30c76d95ea0f9ee06df9d270cca0979
Ishika-om19 <somi37987@gmail.com>
2026-09-09 19:38:50 +0530

    Adding other route and dynamic url

--- Flask/app.py
+++ Flask/app.py
@@ -16,5 +16,9 @@ def showmessage():
 def lab():
     return render_template("lab.html")
 
+@app.route('/home/<uname>')
+def home1(uname):
+    return ("<Marquee width=100% Size=14 BgColor=Pink><Font Color=Red>Welcome To Orange</Font></Marquee><Br><Br><B><Font Size=6 Color=Orange>Welcome : "+uname+"</Font></B>")
+
 if __name__=='__main__':
     app.run(debug=True) #Flask Application will be Started
